3452f41e7405a76cfb5b6011e6c922c86b0a36be
Aditya Chadalwada <adityac564@gmail.com>
2017-07-27 16:00:47 +0530

    Plotting results in plotly
    check the demo plot on https://plot.ly/~adityac564/40/#plot

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,8 +1,16 @@
+import plotly as py
+py.tools.set_credentials_file(username='adityac564', api_key='r0oOvo7yWphavCbFEoMG') # plotly username and API key
+import plotly.plotly as py
+import plotly.graph_objs as go
+import plotly.tools as plotly_tools
+from plotly.graph_objs import *
+py.sign_in("adityac564", "r0oOvo7yWphavCbFEoMG")
+
 import re
 import tweepy
 from tweepy import OAuthHandler
 from textblob import TextBlob
- 
+
 class TwitterClient(object):
     '''
     Generic Twitter Class for sentiment analysis.
@@ -12,10 +20,10 @@ class TwitterClient(object):
         Class constructor or initialization method.
         '''
         # keys and tokens from the Twitter Dev Console
-        consumer_key = 'xxxxxxxxx'
-        consumer_secret = 'xxxxxxxxxxxxxxxxxxxxxx'         #your API credentials
-        access_token = 'xxxxxxxxxxxxxxxxxxxxxx'
-        access_token_secret = 'xxxxxxxxxxxxxxxxxxxxxxx'
+        consumer_key = 'Dl6Xl0Xh3ydmcV9bw3Pvr94B4'                              # Twitter API key and access token
+        consumer_secret = 'flrjl36HbaLj3s2zFo8Mzkxv9Lzbmx7KcQLwDlYbDcsegZj91G'
+        access_token = '809633084370886657-He6d90EMjqSEfU87A7a0EmFcl1T9Yo9'
+        access_token_secret = 'Cv37nNYgIDBvdn0KBiVS0CAhTftQ1bzrflSwyz4Q8jjFU'
  
         # attempt authentication
         try:
@@ -89,8 +97,8 @@ class TwitterClient(object):
 def main():
     # creating object of TwitterClient Class
     api = TwitterClient()
-    # calling function to get tweets
-    tweets = api.get_tweets(query = 'OnePlus', count = 200)
+    # calling function to get tweets                          here the query is OnePlus change it to your requirement  
+    tweets = api.get_tweets(query = 'OnePlus', count = 200)  # you can increase the count for more acurate result 
  
     # picking positive tweets from tweets
     ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
@@ -102,10 +110,18 @@ def main():
     print("Negative tweets percentage: {} %".format(100*len(ntweets)/len(tweets)))
     # percentage of neutral tweets
     nutweets = len(tweets) - len(ntweets) - len(ptweets)
-    #nutweets = [tweet for tweet in tweets if tweet['sentiment'] != 'negative' and 'positive']
+    
     print("Neutral tweets percentage: {} %".format(100*nutweets/len(tweets)))
   
-
+    ptweetper = 100*len(ptweets)/len(tweets)
+    ntweetper = 100*len(ntweets)/len(tweets)
+    nutweetper = 100*nutweets/len(tweets)
+    
+    labels = ['Positive_tweets','Negative_tweets','Neutral_Tweets']
+    values = [ptweetper,ntweetper,nutweetper]       #you can see a demo pie chart at 
+    trace = go.Pie(labels=labels, values=values)    #https://plot.ly/~adityac564/40/#plot
+    py.iplot([trace], filename='sentiment_analysis_OnePlus')
+   
  
 if __name__ == "__main__":
     # calling main function
